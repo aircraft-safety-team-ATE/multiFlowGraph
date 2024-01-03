0a0fe9bb80b1ac7051c0d89121ef16e19345a8fa
fzy1999 <921797341@qq.com>
2024-01-03 10:26:23 +0800

    fix:在根系统下删除输入和输出节点不会报错

--- platform/front-end/src/components/logicflowConfig/tools/menu.js
+++ platform/front-end/src/components/logicflowConfig/tools/menu.js
@@ -49,26 +49,28 @@ const menu = {
 
               // 根据子系统的id找到父系统
               let parent_id = this.G_DATA.SystemData.find(item => item.system_id == this.G_DATA.currentSystemId).parent_id
-              let parent_system = this.G_DATA.SystemData.find(item => item.system_id == parent_id)
+              if (parent_id != null) {
+                let parent_system = this.G_DATA.SystemData.find(item => item.system_id == parent_id)
 
-              // 更新parent_system的中对应子系统的input或output
-              let subsystem_node = parent_system.data.nodes.find(item => item.properties.SubsystemId == this.G_DATA.currentSystemId)
-              subsystem_node.properties.fields.input = input_nodes.length
-              // 更新parent_system的中对应子系统的input或output连线的锚点数据
-              // 1. 删除相关连线
-              let input_anchors = subsystem_node.anchors.filter(item => item.type == "left")
-              let delete_edge_index = parent_system.data.edges.findIndex(item => item.targetAnchorId == input_anchors[node.properties.index - 1].id)
-              if (delete_edge_index != -1) {
-                parent_system.data.edges.splice(delete_edge_index, 1)
-              }
-              // 2. 重排其他连线的顺序
-              for (let i = node.properties.index; i < input_anchors.length; i++) {
-                let edge_index = parent_system.data.edges.findIndex(item => item.targetAnchorId == input_anchors[i].id)
-                if (edge_index != -1) {
-                  parent_system.data.edges[edge_index].targetAnchorId = input_anchors[i - 1].id
+                // 更新parent_system的中对应子系统的input或output
+                let subsystem_node = parent_system.data.nodes.find(item => item.properties.SubsystemId == this.G_DATA.currentSystemId)
+                subsystem_node.properties.fields.input = input_nodes.length
+                // 更新parent_system的中对应子系统的input或output连线的锚点数据
+                // 1. 删除相关连线
+                let input_anchors = subsystem_node.anchors.filter(item => item.type == "left")
+                let delete_edge_index = parent_system.data.edges.findIndex(item => item.targetAnchorId == input_anchors[node.properties.index - 1].id)
+                if (delete_edge_index != -1) {
+                  parent_system.data.edges.splice(delete_edge_index, 1)
+                }
+
+                // 2. 重排其他连线的顺序
+                for (let i = node.properties.index; i < input_anchors.length; i++) {
+                  let edge_index = parent_system.data.edges.findIndex(item => item.targetAnchorId == input_anchors[i].id)
+                  if (edge_index != -1) {
+                    parent_system.data.edges[edge_index].targetAnchorId = input_anchors[i - 1].id
+                  }
                 }
               }
-
 
 
               // 3. 重新渲染页面
@@ -88,27 +90,28 @@ const menu = {
 
               // 根据子系统的id找到父系统
               let parent_id = this.G_DATA.SystemData.find(item => item.system_id == this.G_DATA.currentSystemId).parent_id
-              let parent_system = this.G_DATA.SystemData.find(item => item.system_id == parent_id)
+              if (parent_id != null) {
+                let parent_system = this.G_DATA.SystemData.find(item => item.system_id == parent_id)
 
-              // 更新parent_system的中对应子系统的input或output
-              let subsystem_node = parent_system.data.nodes.find(item => item.properties.SubsystemId == this.G_DATA.currentSystemId)
-              subsystem_node.properties.fields.output = output_nodes.length
-              // 更新parent_system的中对应子系统的input或output连线的锚点数据
-              // 1. 删除相关连线
-              let output_anchors = subsystem_node.anchors.filter(item => item.type == "right")
-              let delete_edge_index = parent_system.data.edges.findIndex(item => item.sourceAnchorId == output_anchors[node.properties.index - 1].id)
-              if (delete_edge_index != -1) {
-                parent_system.data.edges.splice(delete_edge_index, 1)
-              }
-              // 2. 重排其他连线的顺序
-              for (let i = node.properties.index; i < output_anchors.length; i++) {
-                let edge_index = parent_system.data.edges.findIndex(item => item.sourceAnchorId == output_anchors[i].id)
-                if (edge_index != -1) {
-                  parent_system.data.edges[edge_index].sourceAnchorId = output_anchors[i - 1].id
+                // 更新parent_system的中对应子系统的input或output
+                let subsystem_node = parent_system.data.nodes.find(item => item.properties.SubsystemId == this.G_DATA.currentSystemId)
+                subsystem_node.properties.fields.output = output_nodes.length
+                // 更新parent_system的中对应子系统的input或output连线的锚点数据
+                // 1. 删除相关连线
+                let output_anchors = subsystem_node.anchors.filter(item => item.type == "right")
+                let delete_edge_index = parent_system.data.edges.findIndex(item => item.sourceAnchorId == output_anchors[node.properties.index - 1].id)
+                if (delete_edge_index != -1) {
+                  parent_system.data.edges.splice(delete_edge_index, 1)
                 }
+                // 2. 重排其他连线的顺序
+                for (let i = node.properties.index; i < output_anchors.length; i++) {
+                  let edge_index = parent_system.data.edges.findIndex(item => item.sourceAnchorId == output_anchors[i].id)
+                  if (edge_index != -1) {
+                    parent_system.data.edges[edge_index].sourceAnchorId = output_anchors[i - 1].id
+                  }
+                }
+
               }
-
-
               // 3. 重新渲染页面
               this.lf.render(graph_data)
 
@@ -324,7 +327,7 @@ const controlConfig = {
   // 故障分析 ※需要与后台交互
   analyse: true  // option value: true | false
 
-  
+
 }
 
 export { menu, controlConfig }
